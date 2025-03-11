f70160aa43af4152963c148e266f94a8622e1a1f
G7-Briguel <2320586@ub.edu.ph>
2025-03-11 21:32:06 +0800

    Add files via upload

---
+++ G7/Barangay_System_G7/Barangay_System_G7/AssistanceForm.vb
@@ -0,0 +1,10 @@
+﻿Public Class AssistanceForm
+    Private AssistanceList As New List(Of String)()
+
+    Private Sub AssistanceForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Form Styling
+        Me.BackColor = Color.FromArgb(232, 245, 233) ' Light Green
+
+    End Sub
+
+End Class
