8586f1c9478a3e93deba878f94466fee9154ad43
G6-Latiza <ernest.latiza@gmail.com>
2025-05-01 19:37:06 +0800

    delete

--- G6/ECreate/My Project/pacitaVENUE.vb
+++
@@ -1,4 +0,0 @@
-﻿Public Class pacitaVENUE
-    Private Sub pacitaVENUE_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-    End Sub
-End Class
--- G6/ECreate/My Project/privateVENUE.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class privateVENUE
-    Private Sub privateVENUE_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-    End Sub
-
-    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles Label10.Click
-
-    End Sub
-End Class
